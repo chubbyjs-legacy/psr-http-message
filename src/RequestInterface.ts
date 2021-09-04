49073c2dbb6588655ae1e46b070f5bfe9ec6d762
Dominik Zogg <dominik.zogg@hotmail.com>
2021-09-04 12:24:21 +0200

    Method is a enum

--- src/RequestInterface.ts
+++ src/RequestInterface.ts
@@ -1,11 +1,23 @@
 import MessageInterface from './MessageInterface';
 import UriInterface from './UriInterface';
 
+export enum Method {
+    CONNECT = 'CONNECT',
+    DELETE = 'DELETE',
+    GET = 'GET',
+    HEAD = 'HEAD',
+    OPTIONS = 'OPTIONS',
+    PATCH = 'PATCH',
+    POST = 'POST',
+    PUT = 'PUT',
+    TRACE = 'TRACE',
+}
+
 interface RequestInterface extends MessageInterface {
     getRequestTarget(): string;
     withRequestTarget(requestTarget: string): this;
     getMethod(): string;
-    withMethod(name: string): this;
+    withMethod(name: Method): this;
     getUri(): UriInterface;
     withUri(uri: UriInterface, preserveHost?: boolean): this;
 }
